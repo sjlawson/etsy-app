774f990c9460b5d07f3a4eaeab241d2302438f86
Samuel Lawson <slawson@mooreheadcomm.com>
2015-01-22 10:38:28 -0500

    Removed redundant migration, cleaned up Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119032155) do
+ActiveRecord::Schema.define(version: 20150119025204) do
 
-  create_table "categories", force: :cascade do |t|
+  create_table "categories", force: true do |t|
     t.string   "name"
     t.string   "thumburl"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "products", force: :cascade do |t|
+  create_table "products", force: true do |t|
     t.string   "product_name"
     t.text     "description"
     t.float    "price"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20150119032155) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
